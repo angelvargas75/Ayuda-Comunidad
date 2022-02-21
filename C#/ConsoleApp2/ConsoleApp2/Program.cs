ea38c39fa9fe7b4674e71361939cc81ce197ac7c
Angel Vargas <angelsk814@gmail.com>
2022-02-21 17:50:23 -0500

    Grupo de Facebook: programa que calcule numeros impares menor que 400 utilizando arreglos

---
+++ C#/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejemplo1();
+            Console.ReadKey();
+        }
+
+        public static void Ejemplo1()
+        {
+            int[] numeros = new int[3];
+            int j = 0;
+            int suma = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Ingrese el numero " + (j + 1));
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+                j++;
+                if (numeros[i] % 2 != 0)
+                {
+                    if (numeros[i] <= 400)
+                        suma += numeros[i];
+                    else
+                        Console.WriteLine("Es mayor que 400");
+                }
+                else
+                    Console.WriteLine("Es numero par");
+            }
+            Console.WriteLine("\nSUMA: " + suma);
+        }
+    }
+}
